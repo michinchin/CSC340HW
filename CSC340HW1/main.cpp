c367e418f4d248c3ab0352efdc239aee24459044
Abigail Chin <chinabigail08@gmail.com>
2017-02-15 13:57:07 -0800

    Initial Commit

---
+++ CSC340HW1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CSC340HW1
+//
+//  Created by Abigail Chin on 2/15/17.
+//  Copyright © 2017 Abigail Chin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
