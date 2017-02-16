56dcad3ccdcd939f2dde3227de77e92b661f32f3
Abigail Chin <chinabigail08@gmail.com>
2017-02-15 19:15:49 -0800

    first function is working

--- CSC340HW1/main.cpp
+++ CSC340HW1/main.cpp
@@ -11,6 +11,7 @@
 #include <string>
 #include <vector>
 
+
 using namespace std;
 
 void getLetterFreq(string s);
@@ -22,16 +23,16 @@ int main(){
     cout << "What string would you like us to evaluate?"<< endl;
     getline(cin, s);
     
-    //    int i = 0;
-    //    char c;
-    //
-    //    while(s[i]){
-    //        c = s[i];
-    //        s[i] = (tolower(c));
-    //        i++;
-    //
-    //    }
-    //
+        int i = 0;
+        char c;
+    
+        while(s[i]){
+            c = s[i];
+            s[i] = (tolower(c));
+            i++;
+    
+        }
+    
     getLetterFreq(s);
     strToToken(s);
     
@@ -40,19 +41,49 @@ int main(){
 
 void getLetterFreq(string s){
     //lower case letters a-z are in ascii code 97-122
-    int freq[26];//initialize array of 26 ints
+    int freq[26] = {};//initialize array of 26 ints
     char letter[26] = {'a','b','c', 'd', 'e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-    int cnt = 0;
-    
-    for(int i = 0; i < s.size(); i++){
-        if(s[i] == letter[i]){
-            cnt++;
-            freq[i] = cnt;
-            cout << "'"<<letter[i]<<"': "<<freq[i]<< ", ";
+
+    for(int i = 0; i < s.size(); ){
+        if((s[i] >= 'a') || (s[i] <= 'z')){
+            for(int k = 0; k < 26;){
+                if(s[i] == letter[k]){
+                    freq[k]++;
+                    i++;
+                    break;
+                }
+                else if (s[i] != letter[k]){
+                    k++;
+                }
+            }
+
         }
-        
     }
-}
+    for(int j = 0; j< 26; j++){
+        cout << "'"<<letter[j]<<"': "<<freq[j]<< ", ";
+    }
+//    while(cnt <= 0){
+//        if(s[i] != letter[k]){
+//            k++;
+//            getLetterFreq(s);
+//        }else if(s[i] == letter[k]){
+//            freq[i]++;
+//            cout << "'"<<letter[i]<<"': "<<freq[i]<< ", ";
+//            cnt++;
+//        }
+//    }
+//    for(int i = 0; i < s.size(); i++){
+//            while(s[i] != letter[k]){
+//                k++;
+//            }
+//            if(s[i] == letter[k]){
+//                cnt++;
+//                freq[i] =cnt;
+//                cout << "'"<<letter[i]<<"': "<<freq[i]<< ", ";
+//            }
+//        }
+    }
+
 void strToToken(string s){
     
     //    if(s.length == 0){
